14b98be937f2fe9a8695be956cd82a86291dc3ea
CosmicEntity <cosmicentity19@gmail.com>
2023-07-26 00:26:54 +0530

    cors issue fix 3

--- axios.config.js
+++ axios.config.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL: "https://mappinappbackend.onrender.com/api",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
 });
 
 export default Axios;
